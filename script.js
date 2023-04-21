6f540f02bcdc1204097503ee3d8b513ee18e96a1
ares1345 <ares1345istaken@gmail.com>
2023-04-21 10:16:24 +0200

    Finish second part of assignment, commencing testing

--- script.js
+++ script.js
@@ -21,37 +21,71 @@ function playRound(playerSelection, computerSelection) {
     
 
     if (playerSelection.toUpperCase() == "PAPER" && computerSelection == "Scissor") {
+        let isGameWon = 0;
         return `You lose! ${computerSelection} beats ${playerSelection} `;
     }
 
     else if (playerSelection.toUpperCase() == "ROCK" && computerSelection == "Paper") {
+        let isGameWon = 0;
         return `You lose! ${computerSelection} beats ${playerSelection} `;
+        
     }
 
     else if (playerSelection.toUpperCase() == "SCISSOR" && computerSelection == "Rock") {
+        let isGameWon = 0;
         return `You lose! ${computerSelection} beats ${playerSelection} `;
     }
 
     else if (playerSelection.toUpperCase() == computerSelection.toUpperCase()) {
+        let isGameWon = 2;
         return `It's a tie! `;
     }
 
     else if (playerSelection.toUpperCase() == "PAPER" && computerSelection == "Rock") {
+        let isGameWon = 1;
         return `You win! ${playerSelection} beats ${computerSelection}`;
     }
 
     else if (playerSelection.toUpperCase() == "SCISSOR" && computerSelection == "Paper") {
+        let isGameWon = 1;
         return `You win! ${playerSelection} beats ${computerSelection} `;
     }
 
     else if (playerSelection.toUpperCase() == "ROCK" && computerSelection == "Scissor") {
+        let isGameWon = 1;
         return `You win! ${playerSelection} beats ${computerSelection} `;
     }
 
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+function game(playRound) {
+    let winCounter = 0;
+    let loseCounter = 0;
+    for (let i = 1; i<=5; i++) {
+        let playerSelection = prompt("What's your choice? (Rock, Paper, Scissor)? ");
+        let computerSelection = getComputerChoice();
+        playRound(playerSelection, computerSelection)
+        switch (isGameWon) {
+            case 0:
+                loseCounter += 1;
+            case 1:
+                winCounter += 1;
+            case 2:
+                loseCounter += 1;
+                winCounter += 1;
+
+            default:
+                console.log("Error")
+        }
+        
+    }
+
+    if (winCounter > loseCounter) {
+        return(`You won the game! `)
+    }
+    else if (winCounter < loseCounter) {
+        return(`You lost the game! `)
+    }
+}
 
 
