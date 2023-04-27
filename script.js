c8ef5d0395e1971daf9a3bac2bdd64a053e2ee6c
ares1345 <ares1345istaken@gmail.com>
2023-04-27 15:32:27 +0200

    Finish game

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const buttons = document.querySelectorAll('button');
 const result = document.querySelector('.result')
+let score = 0;
+let computerScore = 0;
+
+
+
+
+
 
 buttons.forEach((button) => {
 
@@ -7,47 +14,60 @@ buttons.forEach((button) => {
     button.addEventListener('click', () => {
         switch(playRound(rock.value, getComputerChoice())) {
                     case '0':
+                        computerScore += 1
+                        if (computerScore == 5) {
+                            result.setAttribute('style', 'background-color: red; color: white');
+                            result.textContent = 'You have lost the game! ';
+                            computerScore == 0;
+                            score = 0;
+                            break;
+                        };
                         result.textContent = 'You have lost! ';
+                        console.log(computerScore, score)
                         break;
+
+
+
                     case '1':
+                        score += 1
+                        if (score == 5) {
+                            result.setAttribute('style', 'background-color: green; color: white');
+                            result.textContent = 'You have won the game! ';
+                            computerScore == 0;
+                            score = 0;
+                            break;
+                        };
                         result.textContent = 'You won! ';
+                        
+                        console.log(computerScore, score)
                         break;
                     case '2':
-                        result.textContent = 'Its a tie! '
+                        
+                        if (computerScore == 5) {
+                            result.setAttribute('style', 'background-color: red; color: white');
+                            result.textContent = 'You have lost the game! ';
+                            computerScore == 0;
+                            score = 0;
+                            break;
+                        };
+                        if (score == 5) {
+                            result.setAttribute('style', 'background-color: green; color: white');
+                            result.textContent = 'You have won the game! ';
+                            computerScore == 0;
+                            score = 0;
+                            break;
+                        };
+                        
+                        computerScore += 1;
+                        score += 1;
+                        result.textContent = "It's a tie! "
+                        console.log(computerScore, score)
                         break;
-
-
                 }
             })
     });
   
 
-
-
-
-// const rock = document.querySelector('.rock');
-// const paper = document.querySelector('.paper');
-// const scissor = document.querySelector('.scissor');
-// const result = document.querySelector('.result')
-
-// rock.addEventListener('click', () => {
-//     switch(playRound(rock.value, getComputerChoice())) {
-//         case '0':
-//             result.textContent = 'You have lost! '
-//             break;
-//         case '1':
-//             result.te
-
-//     }
-// })
-
-// paper.addEventListener('click', () => {
-//     console.log(playRound(paper.value, getComputerChoice()))
-// })
-
-// scissor.addEventListener('click', () => {
-//     console.log(playRound(scissor.value, getComputerChoice()))
-// })
 
 
 function getComputerChoice() {
@@ -102,10 +122,7 @@ function playRound(playerSelection, computerSelection) {
 
 }
 
-function game(playRound) {
-    let winCounter = 0;
-    let loseCounter = 0;
-    let isGameWon = 0;
+
     
 
 
@@ -128,116 +145,3 @@ function game(playRound) {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    /* for (let i = 1; i<=5; i++) {
-        let playerSelection = prompt("What's your choice? (Rock, Paper, Scissor)? ");
-        let computerSelection = getComputerChoice();
-
-
-        result = playRound(playerSelection, computerSelection)
-        // console.log(playRound(playerSelection, computerSelection))
-        switch (result) {
-            case '0':
-                loseCounter++;
-                console.log(`You lose! ${computerSelection} beats ${playerSelection} `);
-                break;
-
-            case '1':
-                winCounter++;
-                console.log(`You win! ${playerSelection} beats ${computerSelection} `);
-                break;
-
-            case '2':
-                loseCounter++;
-                winCounter++;
-                console.log(`It's a tie! `);
-                break;
-
-            default:
-                console.log("Error");
-                break;
-        }
-
-
-        console.log(`Your win count is ${winCounter}`);
-        console.log(`Your lose count is ${loseCounter}`);
-        
-    } */
-
-
-    
-    if (winCounter > loseCounter) {
-        return(`You won the game! `)
-    }
-    else if (winCounter < loseCounter) {
-        return(`You lost the game! `)
-    }
-}
-
-console.log(game(playRound))
-
-
-
-// `You lose! ${computerSelection} beats ${playerSelection} `;
-//  `It's a tie! `;
-// `You win! ${playerSelection} beats ${computerSelection} `;
